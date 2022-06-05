00031ffb0c4e546975c5373c3a5aa6a8b1534458
jaeseok-go <gojeasuk2@naver.com>
2022-06-05 12:38:59 +0900

    feat: 프로젝트생성 및 초기화

---
+++ src/test/java/jaeseok/study/boardexample/BoardExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package jaeseok.study.boardexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
